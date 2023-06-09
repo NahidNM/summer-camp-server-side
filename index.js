6e6db8c8a944ac4d346d2d33545fb25a3ea25fa2
NahidNM <mdnahidhasan171@gmail.com>
2023-06-10 02:15:20 +0600

    jwt verify

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
@@ -11,7 +12,7 @@ app.use(cors());
 app.use(express.json())
 
 
-
+// firebase 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jo7sbx1.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,7 +34,19 @@ async function run() {
     const addClassCollection = client.db("summerCamp").collection("addClasses");
     const instuctorsCollection = client.db("summerCamp").collection("insturctor");
 
+    app.post('/jwt', (req, res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 60 * 60 })
+     res.send({token})
+
+    })
+
 // user related apis
+app.get('/users', async(req, res)=>{
+  const result = await usersCollection.find().toArray();
+  res.send(result)
+})
+
 app.post('/users', async(req, res)=>{
   const user = req.body;
 // console.log(user);
@@ -43,8 +56,20 @@ const existingUser = await usersCollection.findOne(quary);
 if(existingUser){
   return res.send({message: 'user already exists'})
 }
+ const result = await usersCollection.insertOne(user);
+  res.send(result)
+})
 
-  const result = await usersCollection.insertOne(user);
+app.patch('/users/admin/:id', async(req, res)=>{
+  const id = req.params.id;
+  const filter = {_id: new ObjectId(id)};
+  const updateDoc = {
+    $set: {
+      role: 'admin'
+    },
+  };
+
+  const result =await usersCollection.updateOne(filter, updateDoc);
   res.send(result)
 })
 
