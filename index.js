749afba63e59da112e7719ab0a420f87e57092e6
NahidNM <mdnahidhasan171@gmail.com>
2023-06-10 04:38:32 +0600

    verify

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
+const verifyJWT = (req, res, next) =>{
+  const authorization =req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unauthorized access'})
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) =>{
+    if(error){
+      return res.status(401).send({error: type, message: 'unauthorized access'})
+    }
+req.decoded= decoded;
+next();
+  })
+}
+
 
 // firebase 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jo7sbx1.mongodb.net/?retryWrites=true&w=majority`;
@@ -36,13 +53,24 @@ async function run() {
 
     app.post('/jwt', (req, res)=>{
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 60 * 60 })
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
      res.send({token})
 
     })
 
+    // varifu Admin
+    const verifyAdmin = async(req, res, next) =>{
+      const email = req.decoded.email;
+      const quary = {email: email};
+      const user = await usersCollection.findOne(quary);
+      if(user?.role == 'admin'){
+        return res.status(403).send({error: true, message: 'forbidden messeage'})
+      }
+      next();
+    }
+
 // user related apis
-app.get('/users', async(req, res)=>{
+app.get('/users', verifyJWT, verifyAdmin, async(req, res)=>{
   const result = await usersCollection.find().toArray();
   res.send(result)
 })
@@ -58,6 +86,18 @@ if(existingUser){
 }
  const result = await usersCollection.insertOne(user);
   res.send(result)
+})
+
+app.get('/users/admin/:email', verifyJWT,  async(req, res) =>{
+  const email = req.params.email;
+
+  if(req.decoded.email !== email){
+    res.send({admin: false})
+  }
+  const quary = {email: email};
+  const user = await usersCollection.findOne(quary);
+  const result = {admin: user?.role === 'admin'}
+  res.send(result);
 })
 
 app.patch('/users/admin/:id', async(req, res)=>{
@@ -94,12 +134,18 @@ app.patch('/users/admin/:id', async(req, res)=>{
     })
 
 // Add class colection
-app.get('/addClasses', async(req, res) =>{
+app.get('/addClasses', verifyJWT, async(req, res) =>{
   const email = req.query.email;
   console.log(email);
   if(!email){
     res.send([])
   }
+
+  const decodedEmail = req.decoded.email;
+  if(email !== decodedEmail){
+    return res.status(401).send({error: true, message: 'forbiden access'})
+  }
+
   const query = {email: email}
   const result = await addClassCollection.find(query).toArray();
   res.send(result)
