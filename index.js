37412d61a2e4e2a0fb6fa7895f528d4dbd59d90f
NahidNM <mdnahidhasan171@gmail.com>
2023-06-12 04:07:12 +0600

    detele user

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
+
 const jwt = require('jsonwebtoken');
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
@@ -50,12 +53,14 @@ async function run() {
     const classesCollection = client.db("summerCamp").collection("classes");
     const addClassCollection = client.db("summerCamp").collection("addClasses");
     const instuctorsCollection = client.db("summerCamp").collection("insturctor");
+    const PaymentCollection = client.db("summerCamp").collection("payments");
+
+// -----------jWT--------
 
     app.post('/jwt', (req, res)=>{
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5h' })
      res.send({token})
-
     })
 
     //---------------- varifu Admin------------
@@ -64,7 +69,7 @@ async function run() {
       const email = req.decoded.email;
       const quary = {email: email};
       const user = await usersCollection.findOne(quary);
-      console.log(user);
+      // console.log(user);
       if(user?.role !== 'admin'){
         return res.status(403).send({error: true, message: 'forbidden messeage'})
       }
@@ -117,7 +122,7 @@ app.delete('/users/:id', async (req, res) => {
 
 // --------------user Admin-------------
 
-app.get('/users/admin/:email', verifyJWT,   async(req, res) =>{
+app.get('/users/admin/:email', verifyJWT,  async(req, res) =>{
   const email = req.params.email;
 
   if(req.decoded.email !== email){
@@ -181,8 +186,11 @@ app.patch('/users/instructor/:id', async(req, res)=>{
       res.send(result);
     })
 
-    app.post('/classes', async(req, res) =>{
-      const newClass =
+    app.post('/classes',  async(req, res) =>{
+      const newClass =req.body;
+      const result = await classesCollection.insertOne(newClass);
+      res.send(result)
+      
     })
 
 
@@ -199,10 +207,10 @@ app.patch('/users/instructor/:id', async(req, res)=>{
 
     
 
-// Add class colection
+// ----------------------Add class in user colection-------------------
 app.get('/addClasses', verifyJWT, async(req, res) =>{
   const email = req.query.email;
-  console.log(email);
+  // console.log(email);
   if(!email){
     res.send([])
   }
@@ -223,6 +231,8 @@ app.post('/addClasses', async (req, res) => {
   res.send(result);
 })
 
+
+
 app.delete('/addClasses/:id', async (req, res) => {
   const id = req.params.id;
   const query = { _id: new ObjectId(id) };
@@ -230,9 +240,83 @@ app.delete('/addClasses/:id', async (req, res) => {
   res.send(result);
 })
 
+
+// -----------Creat Payment sectors and Single addClasse data get----------
+
+app.get('/addClasses/:id', async (req, res) => {
+  const id = req.params.id;
+const quary = {_id: new ObjectId(id)}
+  const result = await addClassCollection.findOne(quary);
+  res.send(result);
+})
+
+app.post('/create-payment-intent', verifyJWT, async(req, res) =>{
+  const {price} = req.body;
+  const amount =parseInt(price *100);
+  
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: 'usd',
+    payment_method_types: ['card']
+  });
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  })
+
+})
+
+
+// ------------------Payment database and update set ------------
+
+// class update
+app.get('/classupdatedata/:id', async(req, res)=>{
+  const id =req.params.id;
+  const quary = {_id: new ObjectId(id)};
+  const result = await classesCollection.findOne(quary);
+  res.send(result);
+} )
+
+app.put('/classupdatedata/:id', async (req, res) =>{
+  const id =req.params.id;
+  const filter = {_id: new ObjectId(id)};
+  const option = { upsert: true };
+  const update = req.body;
+  const updateDoc = {
+    $set: {
+      available_seats: update.newseat,
+      enroll: update.newEnroll,
+    }
+  }
+
+  const result = await classesCollection.updateOne(filter, updateDoc, option);
+  res.send(result);
+})
+
+
+// ----------------------Enroll database-------------
+
+app.get('/enrollclass', async (req, res) => {
+  const result = await PaymentCollection.find().toArray();
+  res.send(result);
+})
+
+app.post('/payment',  async(req, res)=>{
+  const payment = req.body;
+  // console.log(payment);
+  const insertResult =await PaymentCollection.insertOne(payment);
+
+  const id=payment.classId;
+  const quary = {_id: {$in: [new ObjectId(id)]}};
+  const deleteResult = await addClassCollection.deleteOne(quary);
+  res.send({insertResult, deleteResult});
+})
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log("Pinged your deployment. You successfully connected to MongoDB!")
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
